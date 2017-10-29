3e6237e751135b001e23789a86021529412e3170
Nicolas Cloutier <nic007@live.fr>
2017-10-29 17:30:25 -0400

    Changed the flow for an async connexion

--- app/SourceList/actions/ConnectSource.action.js
+++ app/SourceList/actions/ConnectSource.action.js
@@ -1,70 +1,6 @@
-// import { enqueue } from './../../Player/player.actions';
-import Axios from 'axios';
-
-/*
-const electron = window.require('electron').remote;
-
-require('electron-reload')(__dirname);
-require('babel-polyfill');
-
-// Module to control application life.
-const { app } = electron.app;
-
-// Module to create native browser window.
-const { BrowserWindow } = electron.BrowserWindow;
-*/
-
 class ConnectSource {
-  test(source, authUrl, authCompleted) {
-    const { remote } = window.require('electron');
-    const { BrowserWindow } = remote;
-
-    let authWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      show: false,
-      'node-integration': false,
-      'web-security': false,
-    });
-    authWindow.loadURL(authUrl);
-    authWindow.show();
-    // 'will-navigate' is an event emitted when the window.location changes
-    // newUrl should contain the tokens you need
-    authWindow.webContents.on('will-navigate', (event, newUrl) => {
-      if (newUrl.startsWith('https://www.foo.bar/oauth2/callback')) {
-        authCompleted(source, newUrl);
-        authWindow.close();
-      }
-    });
-
-    authWindow.on('closed', () => {
-      authWindow = null;
-    });
-  }
-
-  onAuthorizationCompleted(source, strUrl) {
-    // const index = url.search('code=');
-    const url = new URL(strUrl);
-    const code = url.searchParams.get('code');
-    const tokenUri = source.getToken(code);
-    console.log(tokenUri);
-
-    const headers = {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: source.authorizationHeader,
-    };
-
-    Axios.post(tokenUri, null, { headers })
-      .then((response) => {
-        console.log(response.data.access_token);
-        // dispatch('foo');
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  execute(state, proxyName) {
+  execute(state, proxy, connectionStatus) {
+    /*
     let sources = state.get('sources');
     const sourceKey = sources.findKey(value => value.name === proxyName);
     const source = sources.get(sourceKey);
@@ -79,10 +15,15 @@ class ConnectSource {
 
         console.log(response);
       })
-      */
-    }
 
-    return state.set('sources', sources);
+    }
+    */
+
+    // TODO:: Change proxy status to connection
+    console.log(proxy);
+    console.log(connectionStatus);
+    return state;
+    // return state.set('sources', sources);
   }
 }
 
--- app/SourceList/sourcelist.actions.js
+++ app/SourceList/sourcelist.actions.js
@@ -1,7 +1,76 @@
+import Axios from 'axios';
+import { Mutex } from 'await-semaphore';
+
+async function fetchAuthorizationCode(source) {
+  let resultUrl = null;
+
+  const { remote } = window.require('electron');
+  const { BrowserWindow } = remote;
+
+  const mutex = new Mutex();
+  await mutex.acquire();
+  // Result = null
+
+  let authWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    'node-integration': false,
+    'web-security': false,
+  });
+  authWindow.loadURL(source.authorizationUrl);
+  authWindow.show();
+  // 'will-navigate' is an event emitted when the window.location changes
+  // newUrl should contain the tokens you need
+  authWindow.webContents.on('will-navigate', (event, newUrl) => {
+    if (newUrl.startsWith(source.redirectUri)) {
+      resultUrl = newUrl;
+      authWindow.close();
+    }
+  });
+
+  authWindow.on('closed', () => {
+    authWindow = null;
+
+    release();
+  });
+
+  await mutex.acquire();
+  return resultUrl;
+}
+
+async function fetchTokenCode(source, strUrl) {
+  // const index = url.search('code=');
+  const url = new URL(strUrl);
+  const code = url.searchParams.get('code');
+  const tokenUri = source.getToken(code);
+  console.log(tokenUri);
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: source.authorizationHeader,
+  };
+
+  const response = await Axios.post(tokenUri, null, { headers });
+  return response.data.access_token;
+}
+
 // DO NOT PUT IN DEFAULT OR IT WON'T WORK
-export function connectSource(proxyName) {
+export const CONNECT_SOURCE = 'CONNECTSOURCE';
+export function connectSource(proxy) {
+  return (dispatch) => {
+    console.log(dispatch);
+    // dispatch(START_CONNECTION);
+    return fetchAuthorizationCode(proxy)
+      .then(response => fetchTokenCode(proxy, response))
+      .then(response => console.log(response) /* dispatch connection completed */);
+    // On error dispatch connexion failed
+  };
+
+  /*
   return {
-    type: 'CONNECTSOURCE',
+    type: CONNECT_SOURCE,
     proxyName,
   };
+  */
 }
