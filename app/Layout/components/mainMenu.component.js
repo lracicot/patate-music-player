e179d1c350320e51af246baa721b58a480d10bb0
Louis Racicot <info@louisracicot.com>
2017-10-29 16:27:18 -0400

    wip

--- app/Layout/components/mainMenu.component.js
+++ app/Layout/components/mainMenu.component.js
@@ -1,12 +1,17 @@
 import React, { PureComponent } from 'react';
 import { Input, Menu } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+<<<<<<< HEAD
 import { withRouter } from 'react-router-dom';
+=======
+import { NavLink } from 'react-router-dom';
+>>>>>>> wip
 
 
 export default class MainMenu extends PureComponent {
   render() {
     const { activeItem } = this.props;
+<<<<<<< HEAD
 
     const HomeLink = withRouter(({ history }) => (
       <Menu.Item
@@ -32,6 +37,18 @@ export default class MainMenu extends PureComponent {
       <Menu vertical>
         <HomeLink />
         <SourceLink />
+=======
+    return (
+      <Menu vertical>
+        <Menu.Item name="home" to="/" as={NavLink} active={activeItem === 'home'} >
+          Home
+        </Menu.Item>
+
+        <Menu.Item to="/sources" as={NavLink} active={activeItem === 'sources'} >
+          Music services
+        </Menu.Item>
+
+>>>>>>> wip
         <Menu.Item>
           <Input icon="search" placeholder="Search song..." />
         </Menu.Item>
