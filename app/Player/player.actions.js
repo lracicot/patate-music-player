506f52efeda854f1d1ec091b42d6a78ea79ed7e6
Louis Racicot <info@louisracicot.com>
2017-11-03 12:02:45 -0400

    javadoc

--- app/Player/player.actions.js
+++ app/Player/player.actions.js
@@ -1,21 +1,47 @@
 
+/**
+ * Creates an action named "TOGGLEPLAY" with
+ * the required data to execute the reducer action.
+ *
+ * @return ActionCreator the action creater
+ */
 export function toggleplay() {
   return {
     type: 'TOGGLEPLAY',
   };
 }
+
+/**
+ * Creates an action named "PLAY" with
+ * the required data to execute the reducer action.
+ *
+ * @return ActionCreator the action creater
+ */
 export function play() {
   return {
     type: 'PLAY',
   };
 }
 
+/**
+ * Creates an action named "STOP" with
+ * the required data to execute the reducer action.
+ *
+ * @return ActionCreator the action creater
+ */
 export function stop() {
   return {
     type: 'STOP',
   };
 }
 
+/**
+ * Creates an action named "PLAYING" with
+ * the required data to execute the reducer action.
+ *
+ * @param audioStatus The currect status (time elapsed, etc.) of the dom player
+ * @return ActionCreator the action creater
+ */
 export function playing(audioStatus) {
   return {
     type: 'PLAYING',
@@ -24,9 +50,9 @@ export function playing(audioStatus) {
 }
 
 /**
-   * Create an action to enqueue a list of tracks
-   * @param {List} tracks - List of tracks to enqueue.
-   */
+ * Create an action to enqueue a list of tracks
+ * @param {List} tracks - List of tracks to enqueue.
+ */
 export function enqueue(tracks = null) {
   return {
     type: 'ENQUEUE',
@@ -34,18 +60,36 @@ export function enqueue(tracks = null) {
   };
 }
 
+/**
+ * Creates an action named "CLEAR_QUEUE" with
+ * the required data to execute the reducer action.
+ *
+ * @return ActionCreator the action creater
+ */
 export function clearQueue() {
   return {
     type: 'CLEAR_QUEUE',
   };
 }
 
+/**
+ * Creates an action named "NEXT" with
+ * the required data to execute the reducer action.
+ *
+ * @return ActionCreator the action creater
+ */
 export function next() {
   return {
     type: 'NEXT',
   };
 }
 
+/**
+ * Creates an action named "PREV" with
+ * the required data to execute the reducer action.
+ *
+ * @return ActionCreator the action creater
+ */
 export function prev() {
   return {
     type: 'PREV',
