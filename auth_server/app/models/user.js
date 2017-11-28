7e7c8c12e8e82c63d2763f7f295d5f69ee20a9cf
Louis Racicot <info@louisracicot.com>
2017-11-28 00:50:07 -0500

    add auth server

---
+++ auth_server/app/models/user.js
@@ -0,0 +1,17 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+const SourceAccess = new Schema({
+    source: { type: String, required: true },
+    accessToken: { type: String, required: true },
+});
+
+const User = new Schema({
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    sources: [SourceAccess]
+});
+
+
+module.exports.SourceAccess = mongoose.model('SourceAccess', SourceAccess);
+module.exports.User = mongoose.model('User', User);
